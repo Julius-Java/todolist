2668ec64292b3817cec5ede15133324d85e29e89
Emmanuel Julius <emmanueljulius9102016@gmail.com>
2023-02-05 00:04:09 +0100

    Remove port number from console log

--- app.js
+++ app.js
@@ -190,5 +190,5 @@ app.get('/:customListName', (req, res) => {
 
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log("Server is running on port 3000 || " + process.env.PORT);
+    console.log("Server up and running");
 });
