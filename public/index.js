e9c9637b0378dc94ed30267fc32ca54260b4904a
Emmanuel Julius <emmanueljulius9102016@gmail.com>
2023-02-05 02:18:10 +0100

    Add menu button click animation

--- public/index.js
+++ public/index.js
@@ -7,3 +7,39 @@ form.submit((e) => {
         alert("Please enter a todo item");
     }
 });
+
+const menuBtn = $('i.fa-solid')
+const sideBar = $('.container')
+
+var menuOpen = false;
+
+menuBtn.click(() => {
+    menuBtn.toggleClass('rotate-half')
+
+    setTimeout(() => {
+        menuBtn.toggleClass('fa-bars') // removes the bar-icon
+        menuBtn.toggleClass('rotate-full fa-arrow-right-long') // rotates 
+    }, 100)
+
+    if (!menuOpen) {
+        sideBar.css({
+          "left": "0.9rem"
+        });
+
+        $("#overlay").css({
+            "display": "block"
+          });
+
+        menuOpen = true;
+      } else {
+        sideBar.css({
+          "left": "-400px"
+        });
+
+        $("#overlay").css({
+            "display": "none"
+          });
+
+        menuOpen = false;
+      }
+})
