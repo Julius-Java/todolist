2cbd827f16802e385140cf96b9d16997e101164a
Emmanuel Julius <emmanueljulius9102016@gmail.com>
2023-02-07 18:21:55 +0100

    Add new routes to node server and handle post requests to them

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 const mongoose = require('mongoose')
 const _ = require('lodash')
 const env = require('dotenv').config()
+const path = require('path');
+const { prototype } = require("events");
 
 const app = express();
 
@@ -61,37 +63,59 @@ const List = mongoose.model("List", listSchema)
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
 
 app.get("/", (req, res) => {
     // const day = date.getDate();
-    Item.find({}, (err, items) => {
-        if (err) {
-            console.log(err);
-        } else {
-            if (items.length === 0) {
-                Item.insertMany(defaultItems, (err) => {
-                    if (err) {
-                        console.log(err);
-                    }
-                    else {
-                        console.log("Successfully added default items to DB")
-                    };
-                })
-                res.redirect('/');
-            } else {
-                res.render("list", {listTitle: "Today", newListItems: items});
-            }
-        }
-    })
 
     List.find({}, (err, items) => {
-        if (err) {
-            console.log(err);
+        if (!err) {
+            var lists = items;
+            Item.find({}, (err, items) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    if (items.length === 0) {
+                        Item.insertMany(defaultItems, (err) => {
+                            if (err) {
+                                console.log(err);
+                            }
+                            else {
+                                console.log("Successfully added default items to DB")
+                            };
+                        })
+                        res.redirect('/');
+                    } else {
+                        res.render("list", {listTitle: "Today", newListItems: items, categories: lists});
+                    }
+                }
+            });
         } else {
-            console.log(items);
+            console.log(err);
         }
-    })
+    });
+
+    // Item.find({}, (err, items) => {
+    //     if (err) {
+    //         console.log(err);
+    //     } else {
+    //         if (items.length === 0) {
+    //             Item.insertMany(defaultItems, (err) => {
+    //                 if (err) {
+    //                     console.log(err);
+    //                 }
+    //                 else {
+    //                     console.log("Successfully added default items to DB")
+    //                 };
+    //             })
+    //             res.redirect('/');
+    //         } else {
+    //             res.render("list", {listTitle: "Today", newListItems: items, categories: lists});
+    //         }
+    //     }
+    // })
+
 });
 
 app.post("/", (req, res) => {
@@ -102,7 +126,11 @@ app.post("/", (req, res) => {
         name: newToDoItem
     })
 
-    if (listName === "Today") {
+    if (!newToDoItem || !listName) {
+        res.redirect('/')
+    }
+
+    else if (listName === "Today") {
         newItem.save()
             .then((result) => {
                 console.log(newItem.name + " todo item has been successfully added to DB");
@@ -111,7 +139,9 @@ app.post("/", (req, res) => {
             .catch((err) => {
                 console.log(err);
             })
-    } else {
+    }
+
+    else {
         List.findOne({name: listName}, function(err, foundItem) {
             if (!err) {
                 foundItem.items.push(newItem);
@@ -165,6 +195,8 @@ app.get("/about", (req, res) => {
     res.render("about");
 })
 
+
+
 app.get('/:customListName', (req, res) => {
     const listName = _.capitalize(req.params.customListName);
 
@@ -188,12 +220,60 @@ app.get('/:customListName', (req, res) => {
                 res.redirect('/' + listName)
             } else {
                 // Display list
-                res.render("list", {listTitle: result.name, newListItems: result.items})
+                List.find({}, (err, categories) => {
+                    if (!err) {
+                        res.render("list", {listTitle: result.name, newListItems: result.items, categories: categories});
+                    } else {
+                        console.log(err);
+                    }
+                });
             }
         } else {
             console.log(err);
         }
     })
+})
+
+app.post('/add-category', (req, res) => {
+    const category = _.capitalize(req.body.newCategory)
+    const listName = req.body.list;
+
+    const list = new List({
+        name: category,
+        items: defaultItems
+    })
+
+    // console.log(category);
+    List.findOne({name: category}, (err, result) => {
+        if (!err) {
+            if (!result) {
+                // console.log("Item not found");
+                list.save()
+                    .then((successful) => {console.log(successful.name + " category saved");})
+                    .catch((failed) => {console.log(failed);})
+                if (listName == "Today") {
+                    res.redirect('/')
+                } else {
+                    res.redirect('/' + listName)
+                }
+            } else {
+                // console.log("Item found");
+                if (listName == "Today") {
+                    res.redirect('/')
+                } else {
+                    res.redirect('/' + listName)
+                }
+            }
+        } else {
+            console.log(err);
+        }
+    })
+})
+
+app.post('/change-category', (req, res) => {
+    const category = req.body.category
+    // console.log(category);
+    res.redirect('/' + category)
 })
 
 
